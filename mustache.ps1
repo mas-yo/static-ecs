2a7836d1beb7a98d01529ea0c68e3780da1acb7a
Masahiro Yoshida <myoshida@aiming-inc.com>
2025-04-06 17:21:47 +0900

    difinition of components

--- mustache.ps1
+++ mustache.ps1
@@ -19,8 +19,23 @@ $ConvertTypeTable = Get-Content -Path .\convert-type-$Language.yaml -Raw | Conve
 
 $SystemsDifinitionFile = Get-Content -Path $Path -Raw
 
-$SystemDifinition = ConvertFrom-MustacheTemplate -Template $SystemsDifinitionFile -Values $ConvertTypeTable
+$BaseDifinition = ConvertFrom-MustacheTemplate -Template $SystemsDifinitionFile -Values $ConvertTypeTable | ConvertFrom-Yaml
 
-$Source = Get-Content -Path .\static-ecs.fs.template
+$Source = Get-Content -Path .\static-ecs.fs.template -Raw
+
+# echo $SystemDifinition.Components.GetType()
+
+$SystemDifinition = @{}
+$SystemDifinition.Components = $BaseDifinition.Components.Keys #[System.Collections.Generic.List[string]]::new()
+
+$SystemDifinition.ValueComponents = $BaseDifinition.Components.GetEnumerator()
+ | Where-Object { $_.Value -is [string] }
+ | ForEach-Object { @{ Name=$_.Key; Content=$_.Value} }
+
+$SystemDifinition.StructComponents = $BaseDifinition.Components.GetEnumerator()
+ | Where-Object { $_.Value -is [hashtable] }
+ | ForEach-Object { @{ Name=$_.Key; Content=$_.Value} }
+
+# echo $SystemDifinition.ValueComponents
 
 ConvertFrom-MustacheTemplate -Template $Source -Values $SystemDifinition
