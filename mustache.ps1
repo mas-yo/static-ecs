8072f20bbc6c4e9052c5d62398c94e1efe287939
Masahiro Yoshida <myoshida@aiming-inc.com>
2025-04-25 14:10:58 +0900

    arrange yaml format

--- mustache.ps1
+++ mustache.ps1
@@ -23,24 +23,26 @@ function ConvertToNameAndType($kv) {
 
 $Language = [Language]$Language
 
-$ConvertTypeTable = Get-Content -Path .\convert-type-$Language.yaml -Raw | ConvertFrom-Yaml
+# $ConvertTypeTable = Get-Content -Path .\convert-type-$Language.yaml -Raw | ConvertFrom-Yaml
 
-$BaseDifinition = Get-Content -Path $Path -Raw | ConvertFrom-Yaml
+$SystemDifinition = Get-Content -Path $Path -Raw | ConvertFrom-Yaml
+
+echo $SystemDifinition
 
 $Source = Get-Content -Path .\static-ecs.fs.template -Raw
 
 # echo $SystemDifinition.Components.GetType()
 
-$SystemDifinition = @{}
-$SystemDifinition.Components = $BaseDifinition.Components.Keys
+# $SystemDifinition = @{}
+# $SystemDifinition.Components = $BaseDifinition.Components.Keys
 
-$SystemDifinition.ValueComponents = $BaseDifinition.Components.GetEnumerator()
- | Where-Object { $_.Value -is [string] }
- | ForEach-Object { ConvertToNameAndType $_ }
+# $SystemDifinition.ValueComponents = $BaseDifinition.Components.GetEnumerator()
+#  | Where-Object { $_.Value -is [string] }
+#  | ForEach-Object { ConvertToNameAndType $_ }
 
-$SystemDifinition.StructComponents = $BaseDifinition.Components.GetEnumerator()
- | Where-Object { $_.Value -is [hashtable] }
- | ForEach-Object { @{ Name=$_.Key; Fields=($_.Value.GetEnumerator() | ForEach-Object { ConvertToNameAndType $_ }) } }
+# $SystemDifinition.StructComponents = $BaseDifinition.Components.GetEnumerator()
+#  | Where-Object { $_.Value -is [hashtable] }
+#  | ForEach-Object { @{ Name=$_.Key; Fields=($_.Value.GetEnumerator() | ForEach-Object { ConvertToNameAndType $_ }) } }
 
 # echo $SystemDifinition.ValueComponents
 
